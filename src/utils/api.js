459e1cafdfceca0c908a7ddfa4847f7ca210623b
Jose Teno <josetrwork@gmail.com>
2025-02-25 12:08:42 -0600

    Api y sus funciones

---
+++ src/utils/api.js
@@ -0,0 +1,13 @@
+const API_URL = 'https://jsonplaceholder.typicode.com/';
+export default async function getPost() {
+    const response = await fetch(`${API_URL}/posts`);
+    return response.json();
+}
+export async function getUser(user_id) {
+    const response = await fetch(`${API_URL}/users/${user_id}`);
+    return response.json();
+}
+export async function getUserPosts(userId = 1) {
+    const response = await fetch(`${API_URL}/posts?userId=${userId}`);
+    return response.json();
+}
